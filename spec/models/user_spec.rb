0273ffbbe7f34a5cd15884a84f3b159fa985b2c3
suzuki pri <pg.econ4869@gmail.com>
2020-04-07 00:29:39 +0900

    🎉Complete model spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,6 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
+
   # 姓、名、メール、パスワードがあれば有効な状態であること
   it "is valid with a first name, last name, email, and password" do
     user = User.new(
@@ -11,24 +12,28 @@ RSpec.describe User, type: :model do
     )
     expect(user).to be_valid
   end
+
   # 名がなければ無効な状態であること
   it "is invalid without a first name" do
     user = User.new(first_name: nil)
     user.valid?
     expect(user.errors[:first_name]).to include("can't be blank")
   end
+
   # 姓がなければ無効な状態であること
   it "is invalid without a last name" do
     user = User.new(first_name: nil)
     user.valid?
     expect(user.errors[:last_name]).to include("can't be blank")
   end
+
   # メールアドレスがなければ無効な状態であること
   it "is invalid without an email address" do
     user = User.new(first_name: nil)
     user.valid?
     expect(user.errors[:email]).to include("can't be blank")
   end
+
   # 重複したメールアドレスなら無効な状態であること
   it "is invalid with a duplicate email address" do
     User.create(
@@ -46,6 +51,7 @@ RSpec.describe User, type: :model do
     user.valid?
     expect(user.errors[:email]).to include("has already been taken")
   end
+
   # ユーザーのフルネームを文字列として返すこと
   it "returns a user's full name as a string" do
     user = User.new(
