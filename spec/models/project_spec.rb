7e29d43eee9a609f3891c7c0a1b3e969436d13d5
suzuki pri <pg.econ4869@gmail.com>
2020-04-09 02:07:31 +0900

    🎉Complete test data

--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -2,49 +2,34 @@ require 'rails_helper'
 
 RSpec.describe Project, type: :model do
   it "does not allow duplicate project names per user" do
-    user = User.create(
-      first_name: "Joe",
-      last_name:  "Tester",
-      email:      "joetester@example.com",
-      password:   "dottle-nouveau-pavilion-tights-furze",
-    )
+    user = FactoryBot.create(:user)
 
     user.projects.create(
       name: "Test Project",
     )
-
     new_project = user.projects.build(
       name: "Test Project",
     )
-
     new_project.valid?
     expect(new_project.errors[:name]).to include("has already been taken")
   end
 
   it "allows two users to share a project name" do
-    user = User.create(
-      first_name: "Joe",
-      last_name:  "Tester",
-      email:      "joetester@example.com",
-      password:   "dottle-nouveau-pavilion-tights-furze",
-    )
-
+    user = FactoryBot.create(:user)
     user.projects.create(
       name: "Test Project",
     )
-
-    other_user = User.create(
-      first_name: "Jane",
-      last_name:  "Tester",
-      email:      "janetester@example.com",
-      password:   "dottle-nouveau-pavilion-tights-furze",
-    )
-
+    other_user = FactoryBot.create(:user)
     other_project = other_user.projects.build(
       name: "Test Project",
     )
-
     expect(other_project).to be_valid
+  end
+
+  # たくさんのメモが付いていること
+  it "can have many notes" do
+    project = FactoryBot.create(:project, :with_notes)
+    expect(project.notes.length).to eq 5
   end
 
   # 遅延ステータス
