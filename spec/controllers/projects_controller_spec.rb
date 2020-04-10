1e27f5ceb0a481edd97d0f407e5cea21e756a1f6
suzuki pri <pg.econ4869@gmail.com>
2020-04-10 19:31:42 +0900

    🎉Complete コントローラテスト

---
+++ spec/controllers/projects_controller_spec.rb
@@ -0,0 +1,109 @@
+require 'rails_helper'
+
+RSpec.describe ProjectsController, type: :controller do
+  describe "#index" do
+    # 認証済みのユーザーとして
+    context "as an authenticated user" do  
+      before do
+        @user = FactoryBot.create(:user)
+      end
+
+      # 正常にレスポンスを返す
+      it "responds successfully" do
+        sign_in @user
+        get :index
+        expect(response).to be_success
+      end
+
+      # 200レスポンスを返す
+      it "returns a 200 response" do
+        sign_in @user
+        get :index
+        expect(response).to have_http_status "200"
+      end
+    end
+
+    # ゲストとして
+    context "as a geust" do
+      # 302リクエストを返すこと
+      it "returns a 302 request" do
+        get :index
+        expect(response).to have_http_status "302"
+      end
+
+      # サインイン画面にリダイレクトすること
+      it "redirects to the sign-in page" do
+        get :index
+        expect(response).to redirect_to "/users/sign_in"
+      end
+    end
+  end
+
+  describe "#show" do
+    # 認可されたユーザーとして
+    context "as an authorized user" do
+      before do
+        @user = FactoryBot.create(:user)
+        @project = FactoryBot.create(:project, owner: @user)
+      end
+
+      # 正常にレスポンスを返すこと
+      it "responds successfuly" do
+        sign_in @user
+        get :show, params: { id: @project.id }
+        expect(response).to be_success
+      end
+    end
+
+    # 認可されていないユーザーとして
+    context "as an unauthorized user" do
+      before do
+        @user = FactoryBot.create(:user)
+        other_user = FactoryBot.create(:user)
+        @project = FactoryBot.create(:project, owner: other_user)
+      end
+
+      # ダッシュボードにリダイレクトすること
+      it "redirects to the dashboard" do
+        sign_in @user
+        get :show, params: { id: @project.id }
+        expect(response).to redirect_to root_path
+      end
+    end
+  end
+
+  describe "#create" do
+    # 認証済みのユーザーとして
+    context "as an authorized user" do
+      before do
+        @user = FactoryBot.create(:user)
+      end
+
+      # プロジェクトを追加できること
+      it "adds a project" do
+        project_params = FactoryBot.attributes_for(:project)
+        sign_in @user
+        expect {
+          post :create, params: { project: project_params }
+        }.to change(@user.projects, :count).by(1)
+      end
+    end
+  end
+
+  describe "#update" do
+    # 認証済みのユーザーとして
+    context "as an authorized user" do
+      before do
+        @user = FactoryBot.create(:user)
+        @project = FactoryBot.create(:project, owner: @user)
+      end
+
+      it "updates a project" do
+        project_params = FactoryBot.attributes_for(:project, name: "New project")
+        sign_in @user
+        patch :update, params: { id: @project.id, project: project_params }
+        expect(@project.reload.name).to eq "New project"
+      end
+    end
+  end
+end
